377b9f33cfa057c9affb6448b9027ea729ca9a0b
Matthew Hurley <mhurley8@asu.edu>
2021-03-19 12:56:40 -0700

    updated docstrings

--- curvatures.py
+++ curvatures.py
@@ -2,11 +2,15 @@ from sympy import *
 import mpmath
 
 def tangent_vectors(x, u, v):
+    '''Computes tangent vectors of surface, returns x_u, x_v as tuple'''
     xu = simplify(diff(x, u))
     xv = simplify(diff(x, v))
     return xu, xv
 
 def unit_normal(x, u, v, simple=True):
+    '''Computes unit normal vector (Gauss map) of surface
+
+    Optional argument simple (True/False) determines whether to simple expression for normal vector not.'''
     xu, xv = tangent_vectors(x, u, v)
     normal = xu.cross(xv)
     norm = normal.norm()
@@ -16,6 +20,7 @@ def unit_normal(x, u, v, simple=True):
         return normal/norm
 
 def first_ff(x, u, v):
+    '''Computes first fundamental form of surface, returns E,F,G as tuple'''
     xu, xv = tangent_vectors(x, u, v)
     E = simplify(xu.dot(xu))
     F = simplify(xu.dot(xv))
@@ -23,6 +28,7 @@ def first_ff(x, u, v):
     return E, F, G
 
 def second_ff(x, u, v):
+    '''Computes second fundamental form of surface, returns e,f,g as tuple'''
     xu, xv = tangent_vectors(x, u, v)
     N = unit_normal(x, u, v)
     xuu = simplify(diff(xu, u))
@@ -36,6 +42,7 @@ def second_ff(x, u, v):
     return e, f, g
 
 def dN_p(x, u, v):
+    '''Computes differential of the Gauss map in 2x2 matrix form'''
     E, F, G = first_ff(x, u, v)
     e, f, g = second_ff(x, u, v)
     fffmatrix = Matrix([[E,F],[F,G]])
@@ -44,16 +51,20 @@ def dN_p(x, u, v):
     return dNtranspose.T
 
 def Gaussian_curvature(x, u, v):
+    '''Computes Gaussian curvature of surface'''
     E, F, G = first_ff(x, u, v)
     e, f, g = second_ff(x, u, v)
     return simplify((e*g-f**2) / (E*G-F**2))
 
 def mean_curvature(x, u, v):
+    '''Computes mean curvature of surface'''
     E, F, G = first_ff(x, u, v)
     e, f, g = second_ff(x, u, v)
     return simplify((1/2)*(e*G-2*f*F+g*E) / (E*G-F**2))
 
 def principal_curvatures(x, u, v):
+    '''Computes principal curvatures of surface;
+    returns two values k_1, k_2 as tuple if k_1 and k_2 are different, otherwise if k_1=k_2 then returns a single value'''
     dN = dN_p(x, u, v)
     try:
         k1, k2 = dN.eigenvals()
